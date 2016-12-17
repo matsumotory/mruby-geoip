3f1a41ee086f999d53168ca3519c9ffb0357f252
pyama86 <pyama@pepabo.com>
2016-12-17 13:43:41 +0900

    Forgetting to erase

--- build_config.rb
+++ build_config.rb
@@ -2,6 +2,5 @@ MRuby::Build.new do |conf|
   toolchain :gcc
   conf.gembox 'default'
   conf.gem File.expand_path(File.dirname(__FILE__))
-  conf.gem :github => 'iij/mruby-io'
   conf.enable_test
 end
