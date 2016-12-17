d8d0ee397966f1f81f4b8375f966fb8fa19780f0
pyama86 <pyama@pepabo.com>
2016-12-17 14:20:29 +0900

    Google's A record varies from country to country

--- test/mrb_geoip.rb
+++ test/mrb_geoip.rb
@@ -3,7 +3,7 @@
 ##
 
 GeoIPdat = '/usr/local/share/GeoLiteCity.dat'
-GeoIPhost = 'www.google.com'
+GeoIPhost = '216.58.200.196'
 
 # from: https://github.com/maxmind/geoip-api-php/blob/master/tests/data/GeoIPOrg.dat
 GeoIPIspdat = '/usr/local/share/GeoIPOrg.dat'
@@ -16,35 +16,35 @@ end
 
 assert("GeoIP#country_code") do
   geoip = GeoIP.new GeoIPdat
-  geoip.record_by_name GeoIPhost
+  geoip.record_by_addr GeoIPhost
 
   assert_equal("US", geoip.country_code)
 end
 
 assert("GeoIP#region") do
   geoip = GeoIP.new GeoIPdat
-  geoip.record_by_name GeoIPhost
+  geoip.record_by_addr GeoIPhost
 
   assert_equal("CA", geoip.region)
 end
 
 assert("GeoIP#region_name") do
   geoip = GeoIP.new GeoIPdat
-  geoip.record_by_name GeoIPhost
+  geoip.record_by_addr GeoIPhost
 
   assert_equal("California", geoip.region_name)
 end
 
 assert("GeoIP#city") do
   geoip = GeoIP.new GeoIPdat
-  geoip.record_by_name GeoIPhost
+  geoip.record_by_addr GeoIPhost
 
   assert_equal("Mountain View", geoip.city)
 end
 
 assert("GeoIP#postal_code") do
   geoip = GeoIP.new GeoIPdat
-  geoip.record_by_name GeoIPhost
+  geoip.record_by_addr GeoIPhost
 
   assert_equal("94043", geoip.postal_code)
 end
@@ -58,35 +58,35 @@ end
 
 assert("GeoIP#latitude") do
   geoip = GeoIP.new GeoIPdat
-  geoip.record_by_name GeoIPhost
+  geoip.record_by_addr GeoIPhost
 
   assert_equal(37.4192, geoip.latitude.round(4))
 end
 
 assert("GeoIP#longitude") do
   geoip = GeoIP.new GeoIPdat
-  geoip.record_by_name GeoIPhost
+  geoip.record_by_addr GeoIPhost
 
   assert_equal(-122.0574, geoip.longitude.round(4))
 end
 
 assert("GeoIP#metro_code") do
   geoip = GeoIP.new GeoIPdat
-  geoip.record_by_name GeoIPhost
+  geoip.record_by_addr GeoIPhost
 
   assert_equal(807, geoip.metro_code)
 end
 
 assert("GeoIP#area_code") do
   geoip = GeoIP.new GeoIPdat
-  geoip.record_by_name GeoIPhost
+  geoip.record_by_addr GeoIPhost
 
   assert_equal(650, geoip.area_code)
 end
 
 assert("GeoIP#time_zone") do
   geoip = GeoIP.new GeoIPdat
-  geoip.record_by_name GeoIPhost
+  geoip.record_by_addr GeoIPhost
 
   assert_equal("America/Los_Angeles", geoip.time_zone)
 end
